40fce89f32a95342075c53a249eecd374c61d6e4
Hansajaa <nilanahansaja21@gmail.com>
2024-06-07 10:28:50 +0530

    Implement Hero description part

---
+++ src/molecules/HeroDescription.jsx
@@ -0,0 +1,47 @@
+import { HiDownload } from "react-icons/hi";
+import { IoIosArrowForward } from "react-icons/io";
+import HeroHeadings from "../atoms/headings/HeroHeadings";
+import HeroHeadingSpace from "../atoms/spaces/HeroHeadingSpace";
+
+
+function HeroDescription() {
+    return (
+        <>
+           <div>
+              <HeroHeadings content={"Control the device"}/>
+              <HeroHeadingSpace/>
+              
+              <HeroHeadings content={"easily with"}/>
+              <HeroHeadingSpace/>
+
+              <span className="text-purple-500 text-4xl sm:text-5xl md:text-5xl font-bold flex justify-center md:justify-start">
+                {" "}
+                Smarthome
+              </span>
+              
+              <div className="pt-6 sm:pt-8 lg:pt-9">
+                <span className="text-white flex justify-center md:justify-start">
+                  You can now easily control all your home <br />
+                  devices with just smarthome
+                </span>
+              </div>
+
+              <div className="pt-8 sm:pt-12"/>
+
+              <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
+                <button className="text-black bg-white hover:bg-slate-100 px-6 py-3 rounded-xl p-4 flex items-center justify-center">
+                  <span>Get Started</span>{" "}
+                  <IoIosArrowForward className="mt-1 translate-x-3" />
+                </button>
+
+                <button className="text-white bg-transparent border-2 border-white hover:bg-white hover:text-black px-6 py-3 rounded-xl p-4 flex items-center justify-center">
+                  <span>Download</span>{" "}
+                  <HiDownload className="mt-1 translate-x-3" />
+                </button>
+              </div>
+            </div> 
+        </>
+    );
+}
+
+export default HeroDescription;
