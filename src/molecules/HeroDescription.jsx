8ca1343689c883bb05de74f3f8f7695ed79a5693
Hansajaa <nilanahansaja21@gmail.com>
2024-06-07 13:09:04 +0530

    Added Animation using AOS library

--- src/molecules/HeroDescription.jsx
+++ src/molecules/HeroDescription.jsx
@@ -2,33 +2,45 @@ import { HiDownload } from "react-icons/hi";
 import { IoIosArrowForward } from "react-icons/io";
 import HeroHeadings from "../atoms/headings/HeroHeadings";
 import HeroHeadingSpace from "../atoms/spaces/HeroHeadingSpace";
+import Aos from "aos";
+import { useEffect } from "react";
 
 
 function HeroDescription() {
+
+  useEffect(()=>{
+      Aos.init({
+        duration:3000
+      })
+  },[])
+
     return (
         <>
            <div>
-              <HeroHeadings content={"Control the device"}/>
-              <HeroHeadingSpace/>
-              
-              <HeroHeadings content={"easily with"}/>
-              <HeroHeadingSpace/>
+              <div data-aos="fade-right">
+                <HeroHeadings content={"Control the device"}/>
+                <HeroHeadingSpace/>
+                
+                <HeroHeadings content={"easily with"}/>
+                <HeroHeadingSpace/>
 
-              <span className="text-purple-500 text-4xl sm:text-5xl md:text-5xl font-bold flex justify-center md:justify-start">
-                {" "}
-                Smarthome
-              </span>
-              
-              <div className="pt-6 sm:pt-8 lg:pt-9">
-                <span className="text-white flex justify-center md:justify-start">
-                  You can now easily control all your home <br />
-                  devices with just smarthome
+                <span className="text-purple-500 text-4xl sm:text-5xl md:text-5xl font-bold flex justify-center md:justify-start">
+                  {" "}
+                  Smarthome
                 </span>
+                
+                <div className="pt-6 sm:pt-8 lg:pt-9">
+                  <span className="text-white flex justify-center md:justify-start">
+                    You can now easily control all your home <br />
+                    devices with just smarthome
+                  </span>
+                </div>
               </div>
+              
 
               <div className="pt-8 sm:pt-12"/>
 
-              <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
+              <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6" data-aos="fade-in">
                 <button className="text-black bg-white hover:bg-slate-100 px-6 py-3 rounded-xl p-4 flex items-center justify-center">
                   <span>Get Started</span>{" "}
                   <IoIosArrowForward className="mt-1 translate-x-3" />
--- src/templates/HeroSection.jsx
+++ src/templates/HeroSection.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React,{useEffect} from "react";
 import img from "../assets/img1.png";
 import HeroDescription from "../molecules/HeroDescription";
+import Aos from "aos";
+
 
 function HeroSection() {
+
+  useEffect(()=>{
+      Aos.init({
+        duration:3000
+      })
+
+  },[])
+
   return (
     <>
       <div className="px-4">
         <div className="px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20 py-12">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
             {/* Description */}
-            <HeroDescription />
+            <HeroDescription/>
 
             {/* image */}
-            <div className="mt-8 md:-mt-4">
+            <div className="mt-8 md:-mt-4" data-aos="fade-left">
               <img className="w-full" src={img} alt="Smarthome Device" />
             </div>
           </div>
